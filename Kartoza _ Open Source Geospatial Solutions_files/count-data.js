5f45a544e70e13177b6d3e46d89b3beaca49e9d9
Eddie <eddiechristian1@gmail.com>
2016-02-24 17:11:04 -0500

    docker gui howto

---
+++ Kartoza _ Open Source Geospatial Solutions_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"274 http:\/\/kartoza.com\/?p=274","comments":8},{"id":"254 http:\/\/kartoza.com\/?p=254","comments":0},{"id":"301 http:\/\/kartoza.com\/?p=301","comments":5},{"id":"297 http:\/\/kartoza.com\/?p=297","comments":0},{"id":"278 http:\/\/kartoza.com\/?p=278","comments":2},{"id":"247 http:\/\/kartoza.com\/?p=247","comments":0},{"id":"218 http:\/\/kartoza.com\/?p=218","comments":2},{"id":"319 http:\/\/kartoza.com\/?p=319","comments":0},{"id":"235 http:\/\/kartoza.com\/?p=235","comments":0},{"id":"251 http:\/\/kartoza.com\/?p=251","comments":0}]});
+}
